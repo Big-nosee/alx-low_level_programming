2cb9e6de5eb5e0e663acd2e690ccaa2475a653db
Big-nosee <samsonokehi75@gmail.com>
2022-03-14 21:53:28 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints nuumbers zero to nine
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
