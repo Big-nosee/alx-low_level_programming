154381f0c8ef6eb662938d47762c422588df1215
Big-nosee <samsonokehi75@gmail.com>
2022-03-14 18:54:02 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	}
 	else if (n % 10 == 0)
 	{
-		printf("Last digit of %d is %n and is 0\n", n);
+		printf("Last digit of %d is and is 0\n", n);
 	}
 	else if ((n % 10 < 6) != 0)
 	{
