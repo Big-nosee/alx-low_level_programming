4eced80c64e01ef3fa5b1649540de741923ba1b6
Big-nosee <samsonokehi75@gmail.com>
2022-03-15 07:14:41 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit no. separated by ,
+ * Return: 0 always
+*/
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar('\n');
+	}
+	return (0);
+}
