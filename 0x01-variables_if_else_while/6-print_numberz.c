4f06167a7b6adfea0405dbd3f5c94b33adf75c3c
Big-nosee <samsonokehi75@gmail.com>
2022-03-15 06:01:10 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints zero to nine
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c = '9';)
+	{
+		putchar(c);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
