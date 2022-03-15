319fd01e9f14fdff4e1e433ecb04d9aca1b9b32c
Big-nosee <samsonokehi75@gmail.com>
2022-03-15 11:16:33 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int c, d;
 
-	for (c = 0; c <= 9; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (d = c + 1; d <= 9; d++)
+		for (d = c + 1; d <= '9'; d++)
 		{
 			putchar(c);
 			putchar(d);
 
-			if (c == 8 && d == 9)
+			if (c == '8' && d == '9')
 				continue;
 
 			putchar(',');
