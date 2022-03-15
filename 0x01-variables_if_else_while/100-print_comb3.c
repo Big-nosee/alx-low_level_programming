c830a836e72ba101cdc662523683d7eb1ac29d99
Big-nosee <samsonokehi75@gmail.com>
2022-03-15 10:48:13 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,20 +2,21 @@
 
 /**
  * main - prints all possible combo of two different numbers
- * 	in ascending order
+ *	in ascending order
  * Return: 0 if successful
 */
 int main(void)
 {
 	int c, d;
-	for (c = 0; c <= 9; c++)
+	
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (d = c + 1; d < 10; d++)
+		for (d = c + '1'; d <= '9'; d++)
 		{
 			putchar(c);
 			putchar(d);
-			
-			if (c == 8 && d == 9)
+
+			if (c == '8' && d == '9')
 				continue;
 
 			putchar(',');
