412bc173882db168cb9c8f6fbce49a4873cfc042
Big-nosee <samsonokehi75@gmail.com>
2022-03-14 22:04:17 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - prints alphabets in reverse
+ * Return: 0 if successful
+*/
+int main(void)
+{
+	char c;
+
+	for (c <= 'z'; c--)
+	{
+		putchar(c);
+	}
+	{
+		putchar('\n');
+	}
+}
