71daef1f446423646603927873bd611b7a66a5de
Big-nosee <samsonokehi75@gmail.com>
2022-03-14 21:14:40 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all letters except e and q in lowercase
+ * Return: 0 if executed correctly
+*/
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c != 'e''q'; c++)
+	{
+		putchar(c);
+	}
+	{
+		putchad('\n');
+	}
+	return (0);
+}
