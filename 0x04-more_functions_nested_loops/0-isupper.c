c6a904f269ed037df86da5c36683f18039e68cb7
Big-nosee <samsonokehi75@gmail.com>
2022-03-17 10:53:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-isupper.
+ * Auth: Samson Okehi
+ */
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters.
+ * Returns: 1 if c is uppercase
+ * Returns: 0 if otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
