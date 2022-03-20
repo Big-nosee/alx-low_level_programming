601d953e37b66fdb6cc5f372289937ae9a89b7ed
Big-nosee <samsonokehi75@gmail.com>
2022-03-20 05:04:53 -0700

    morenumbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers 0 to 14 ten times
+ */
+void more_numbers(void)
+{
+	int c, d;
+
+	for (d = 0; d <= 9; d++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 1)
+				_putchar((c / 10) + 0);
+			_putchar((c % 10) + 0);
+		}
+		_putchar('\n');
+	}
+}	
